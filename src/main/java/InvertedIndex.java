b6202e0cfa8d1ba6da95bc507b593064cb3a83f6
yixiang <yixiangd1118@gmail.com>
2018-03-16 19:05:03 -0700

    Removed combiner

--- src/main/java/InvertedIndex.java
+++ src/main/java/InvertedIndex.java
@@ -5,7 +5,6 @@ import java.util.TreeMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -16,17 +15,23 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class InvertedIndex {
 
+    /*
+        Mapper:
+        Generate term: docID list.
+        Eg: "a: 51918182"
+     */
     public static class TokenizerMapper
             extends Mapper<LongWritable, Text, Text, Text> {
 
-        private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
         private Text docID = new Text();
 
         public void map(LongWritable key, Text value, Context context
         ) throws IOException, InterruptedException {
             String line = value.toString();
-            updateCurrentDocID(line);
+            boolean isFirstLineOfDoc = updateCurrentDocID(line);
+            if (isFirstLineOfDoc)
+                trimLine(line, value);
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
@@ -39,12 +44,22 @@ public class InvertedIndex {
             int tabIndex = line.indexOf(tab);
             if (tabIndex == -1) return false;
             String newDocID = line.substring(0, tabIndex);
-            docID.set(line.substring(0, tabIndex));
+            docID.set(newDocID);
             return true;
+        }
+
+        private void trimLine(String line, Text value) {
+            String newLine = line.substring(line.indexOf('\t') + 1);
+            value.set(newLine);
         }
     }
 
-    public static class IntSumReducer
+    /*
+        Reducer:
+        Generate term: docID: frequency list (Inverted Index)
+        Eg: "a: 51918182: 34"
+     */
+    public static class IndexReducer
             extends Reducer<Text, Text, Text, Text> {
         private Text result = new Text();
 
@@ -77,9 +92,9 @@ public class InvertedIndex {
         private String getResultString(TreeMap<String, Integer> termFreqMap) {
             StringBuffer resultBuffer = new StringBuffer();
             for (String docId : termFreqMap.keySet()) {
-                if (docId.length() == 0) continue;
                 resultBuffer.append(docId + ":" + termFreqMap.get(docId) + " ");
             }
+//            resultBuffer.append(termFreqMap.toString());
             return resultBuffer.toString();
         }
     }
@@ -89,8 +104,7 @@ public class InvertedIndex {
         Job job = Job.getInstance(conf, "InvertedIndex");
         job.setJarByClass(InvertedIndex.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setReducerClass(IndexReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
